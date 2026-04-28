f47f8d37be11be2204020e4058e09d6cc5e21024
Solomka21 <79266229+Solomka21@users.noreply.github.com>
2026-04-28 21:00:45 +0300

    Update TB6612.py

--- v_1/TB6612.py
+++ v_1/TB6612.py
@@ -2,7 +2,7 @@ from machine import Pin, PWM
 
 class TB6612:
     def __init__(self, pwm_pin, in1_pin, in2_pin, freq=1000):
-        # Инициализация пинов для одного мотора
+        # Инициализация пинов
         self.pwm = PWM(Pin(pwm_pin, Pin.OUT), freq=freq, duty=0)
         self.in1 = Pin(in1_pin, Pin.OUT)
         self.in2 = Pin(in2_pin, Pin.OUT)
@@ -14,7 +14,7 @@ class TB6612:
         self.pwm.duty(0)
 
     def forward(self, speed):
-        # Движение вперед (ограничиваем скорость от 0 до 1023)
+        # Движение вперед 
         speed = min(1023, max(0, speed))
         self.in1.value(1)
         self.in2.value(0)
